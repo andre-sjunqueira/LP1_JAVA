07991073b3e6b876ffc9498119e0f808bf1d68e4
andre-sjunqueira <andresoaresjunqueira14@gmail.com>
2026-03-19 11:22:56 -0300

    feat: adiciona pasta feira ao diretório aula 4

---
+++ aula 4/feira/Fruta.java
@@ -0,0 +1,48 @@
+public class Fruta {
+    String nome;
+    double peso;
+    double valor;
+
+    public Fruta(double peso, double valor, String nome) {
+        this.peso = peso;
+        this.valor = valor;
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+
+    public void estragar(){
+        System.out.println("O " + nome + " estragou");
+    }
+
+    public void comer(){
+        System.out.println("O cliente acabou de comer o " + nome + " que ele comprou");
+    }
+
+    public void madurar(){
+        System.out.println("O" + nome + " ainda nao esta maduro");
+    }
+}
---
+++ aula 4/feira/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    static void main(String[] args) {
+        Fruta f = new Fruta(180,5,"limao");
+
+        System.out.println();
+
+        f.estragar();
+        f.comer();
+        f.madurar();
+
+        System.out.println();
+
+
+        Cliente c1 = new Cliente(1234567892, "Max Verstappen ", 33 );
+        c1.comprar();
+        c1.andar();
+        c1.reclamar();
+
+        System.out.println();
+
+        Funcionario f1 = new Funcionario(123456789,"Ayrton Senna", 1.70);
+        f1.vende();
+        f1.come();
+        f1.tiraduvida();
+    }
+
+}
+
+
